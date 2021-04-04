9ce60ee72ba44775dc3b97e83d157651e37ba187
liufan <liufan@google.com>
2021-04-04 21:43:00 +0800

    update log level for platon

--- chain/platon/startup.sh
+++ chain/platon/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 platonkey genkeypair | tee >(grep "PrivateKey" | awk '{print $2}' > ~/platon-node/data/nodekey) >(grep "PublicKey" | awk '{print $3}' > ~/platon-node/data/nodeid)
 platonkey genblskeypair | tee >(grep "PrivateKey" | awk '{print $2}' > ~/platon-node/data/blskey) >(grep "PublicKey" | awk '{print $3}' > ~/platon-node/data/blspub)
-platon --identity platon --datadir ~/platon-node/data --port 16789 --rpcport 6789 --rpcapi "db,platon,net,web3,admin,personal" --rpc --nodekey ~/platon-node/data/nodekey --cbft.blskey ~/platon-node/data/blskey --verbosity 1 --rpcaddr 127.0.0.1 --syncmode "fast"
+platon --identity platon --datadir ~/platon-node/data --port 16789 --rpcport 6789 --rpcapi "db,platon,net,web3,admin,personal" --rpc --nodekey ~/platon-node/data/nodekey --cbft.blskey ~/platon-node/data/blskey --verbosity 3 --rpcaddr 127.0.0.1 --syncmode "full"
