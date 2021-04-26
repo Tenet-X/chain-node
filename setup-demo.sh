d93d7e795c31f67276d00251a5692636882698c9
liufan <liufan@google.com>
2021-04-26 17:43:47 +0800

    submit demo script

---
+++ setup-demo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -xe
+read -t 30 -p "Please input GKE cluster name: " CLUSTER_NAME
+echo "GKE cluster name: $CLUSTER_NAME"
+
+read -t 30 -p "Please input full node name: " NODE_NAME
+echo "Full node name: $NODE_NAME"
+
+export CLUSTER_NAME=$CLUSTER_NAME
+export NODE_NAME=$NODE_NAME
+
+PROJECT_ID=$(gcloud config get-value project)
+PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PROJECT_NUMBER)")
+GCP_REGION=$(gcloud config get-value compute/region)
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+     --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
+     --role roles/iam.serviceAccountTokenCreator
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
+     --role roles/container.clusterAdmin
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
+     --role roles/container.admin
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
+     --role roles/compute.admin
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
+     --role roles/iam.serviceAccountUser
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
+     --role roles/compute.storageAdmin
+
+gcloud builds submit --config=./cloudbuild-demo.yaml --substitutions=_PROJECT_ID=${PROJECT_ID},_GCP_REGION=${GCP_REGION},_NODE_NAME=${NODE_NAME},_CLUSTER_NAME=${CLUSTER_NAME}
