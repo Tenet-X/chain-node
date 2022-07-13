5b376a93fed73ae79461b8b68a3be09483bab28e
Bin Wu <binwu@google.com>
2022-07-13 10:48:29 +0800

    bumped GKE version to 1.23

--- create-cluster-eth/gke.tf
+++ create-cluster-eth/gke.tf
@@ -17,8 +17,8 @@ variable "gke_num_nodes" {
 resource "google_container_cluster" "primary" {
   name         = var.cluster_name
   location     = var.region
-  node_version = "1.18"
-  min_master_version = "1.18"
+  node_version = "1.23"
+  min_master_version = "1.23"
 
   remove_default_node_pool = true
   initial_node_count       = 1
