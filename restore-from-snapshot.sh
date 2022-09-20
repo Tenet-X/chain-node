a81a379b8d7b63bae84666e5589ac8b432d8db2a
liting <98chenliting@gmail.com>
2022-09-20 16:17:25 +0800

    fix some errors in creating snapshot

--- restore-from-snapshot.sh
+++ restore-from-snapshot.sh
@@ -7,6 +7,7 @@ echo "GKE cluster name: $CLUSTER_NAME"
 # echo "Chain name: $CHAIN_NAME"
 CHAIN_NAME=geth
 NODE_NAME=node1
+SNAPSHOT_NAME=snapshot.09-20-2022
 # read -t 30 -p "Please input full node name: " NODE_NAME
 echo "Full node name: $NODE_NAME"
 
@@ -15,4 +16,4 @@ PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PRO
 GCP_REGION=$(gcloud config get-value compute/region)
 
 
-gcloud builds submit --config=add-node-snap/cloudbuild.yaml --substitutions=_PROJECT_ID=${PROJECT_ID},_GCP_REGION=${GCP_REGION},_NODE_NAME=${NODE_NAME},_CLUSTER_NAME=${CLUSTER_NAME}
+gcloud builds submit --config=add-node-snap/cloudbuild.yaml --substitutions=_PROJECT_ID=${PROJECT_ID},_GCP_REGION=${GCP_REGION},_NODE_NAME=${NODE_NAME},_CLUSTER_NAME=${CLUSTER_NAME},_SNAPSHOT_NAME=${SNAPSHOT_NAME}
